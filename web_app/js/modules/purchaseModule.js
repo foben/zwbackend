d5a40bf78357642bd4cc32056e74ceb57c36f4ad
Andreas <a.loesche@yahoo.de>
2014-05-24 15:53:40 +0200

    moved web app from static to web_app

---
+++ web_app/js/modules/purchaseModule.js
@@ -0,0 +1,12 @@
+define(["jquery", "jquery.bootstrap"],
+    function($) {
+    	var obj = {};
+
+    	obj.init = function(){
+			console.log("init purchase module");
+			
+    	};
+
+        return obj;
+    }
+);
