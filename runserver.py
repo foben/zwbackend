897c539785983ad465e56b3adf5aa4dcc025b4df
Felix Obenauer <f.obenauer@gmail.com>
2014-05-24 13:49:10 +0200

    updated runscript

--- runserver.py
+++ runserver.py
@@ -1,2 +1,7 @@
+import logging
 from zwbackend import app
-app.run(debug=True)
+from logging.handlers import RotatingFileHandler
+handler = RotatingFileHandler('applog.log', maxBytes=10000, backupCount=1)
+handler.setLevel(logging.DEBUG)
+app.logger.addHandler(handler)
+app.run('0.0.0.0', debug=True)
