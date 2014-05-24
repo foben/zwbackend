d878b8377ea83da97b9cd9335f471ddfe360f8a3
Felix Obenauer <f.obenauer@gmail.com>
2014-05-24 15:59:28 +0200

    dummy data in schema

--- zwbackend/schema.sql
+++ zwbackend/schema.sql
@@ -4,15 +4,18 @@ create table store (
   name text not null
 );
 
-insert into store (name) values ('LIDL'),('ALDI');
+insert into store (name) values ('REWE'), ('LIDL'),('ALDI');
 
 drop table if exists purchase;
 create table purchase (
   id integer primary key autoincrement,
-  name text not null,
+  timestamp integer not null,
   storeid integer,
   FOREIGN KEY(storeid) REFERENCES store(id)
 );
+
+insert into purchase (timestamp, storeid) values (1394477761, 1),
+(1400939632, 1), (1400949632, 1);
 
 drop table if exists item;
 create table item (
@@ -26,6 +29,16 @@ create table item (
   FOREIGN KEY(categoryid) REFERENCES category(id)
 );
 
+insert into item (name, price, quantity, purchaseid, categoryid) values
+("beck's", 4.75, 1, 1, 3),
+("brötchen", 0.45, 4, 1, 2),
+("dr. best", 1.99, 1, 1, 2),
+("BIO MEHRKORNBR", 2.25, 1, 1, 1),
+("BLAETTERTEIG", 1.75, 1, 1, 1),
+("JA ! AUFSCHNITT", 1.39, 1, 1, 1),
+("KIWI BIO", 1.29, 1, 1, 1),
+("CHERRYTOMATE BIO", 1.29, 1, 1, 1);
+
 drop table if exists category;
 create table category (
   id integer primary key autoincrement,
@@ -33,3 +46,8 @@ create table category (
   parentid integer,
   FOREIGN KEY(parentid) REFERENCES category(id)
 );
+
+insert into category (name, parentid) values
+("LEBENSMITTEL", null),
+("HYGIENE", null),
+("ALKOHOL", null);
