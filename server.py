f306d9d9cf8f80d94310267c8d179d6b4ea5c333
Felix Obenauer <f.obenauer@gmail.com>
2014-05-24 12:47:10 +0200

    Upload files into upload folder

--- server.py
+++ server.py
@@ -1,9 +1,33 @@
-from flask import Flask
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+        render_template, flash, jsonify, make_response
+
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'jokes.db'),
+    DEBUG=True,
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
 
 @app.route('/')
 def hello_world():
     return '<h1>ZW Backend live!</h1>'
 
+@app.route('/shoppinglist', methods=['GET', 'POST'])
+def upload_image():
+    f = request.files['shoppinglist']
+    app.logger.debug(f)
+    f.save('uploads/' + f.filename)
+    return "success", 200
+
 if __name__ == '__main__':
+    handler = RotatingFileHandler('applog.log', maxBytes=10000, backupCount=1)
+    handler.setLevel(logging.DEBUG)
+    app.logger.addHandler(handler)
     app.run('0.0.0.0')
