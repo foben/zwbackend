4390fdb3d2bb9b4641e394dcc00f19218c85b7b1
bernhardschaefer <bschaefer87@gmx.net>
2014-05-25 11:19:32 +0200

    fix price string

--- zwbackend/helper.py
+++ zwbackend/helper.py
@@ -1,3 +1,6 @@
 
 def to_string_price(price):
-    return str(price)[:4].replace('.',',')
+    price_string = str(price)[:4].replace('.',',')
+    if price_string[-1] is ',':
+        price_string = price_string[:-1]
+    return price_string
